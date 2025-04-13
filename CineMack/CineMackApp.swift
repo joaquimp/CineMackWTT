6523e2161e3e89888109df37c1c0a2e016a2dc7d
Joaquim Pessoa Filho <joaquimp@gmail.com>
2025-04-13 15:34:38 -0300

    Initial Commit

---
+++ CineMack/CineMackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineMackApp.swift
+//  CineMack
+//
+//  Created by Joaquim Pessoa Filho on 13/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CineMackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
